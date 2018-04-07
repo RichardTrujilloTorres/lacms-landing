397c0676165827f0bb4eacb53f5fd905a12ddfd7
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2018-04-07 17:17:15 +0000

    Comment out contact section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -155,9 +155,106 @@ A barebones CMS based upon a RESTful API. Meant to be reuse with easy.
                                 </div>
                             </div>
                         </form>
+                        --}}
+
                     </div>
                 </div>
             </div>
+
+            <div class="section section-contacts" id="when">
+                <div class="row">
+                    <div class="col-md-8 ml-auto mr-auto">
+                        <h2 class="text-center title">When</h2>
+                        <h4 class="text-center description">Developments are sporadical and conditioned by the by-project emerging needs. Here's where your input becomes critical! Drop by <a href="#contact">some lines</a> indicating what would you find useful or <a href="https://github.com/richardtrujillotorres/lacms-api">follow the repo</a> to keep up with the developments.<br>Your feedback will be much appreciated!</h4>
+
+                    </div>
+                </div>
+            </div>
+
+
+            {{--<div class="section section-contacts" id="contact">
+                <div class="row">
+                    <div class="col-md-8 ml-auto mr-auto">
+                        <h2 class="text-center title">Tell me what you think!</h2>
+                        <h4 class="text-center description">The fellow developer is the core focus of this project. <br>Whether you have some features in mind you would like to see come to life or any query regarding the project, go ahead and drop me some lines.</h4>
+
+                        <form class="contact-form" method="POST" action="{{ route('contacts.message') }}">
+                            {{ csrf_field() }}
+
+                            <div class="row">
+
+                                <!-- name -->
+                                <div class="col-md-6">
+                                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                        <label class="bmd-label-floating">Name</label>
+                                        <input type="text" name="name" class="form-control" 
+                                            value="{{ old('name') }}" 
+                                            required>
+
+                                            @if ($errors->has('name'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                            @endif
+                                    </div>
+                                </div>
+
+                                <!-- email -->
+                                <div class="col-md-6">
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <label class="bmd-label-floating">Email*</label>
+                                        <input type="email" name="email" class="form-control" 
+                                            value="{{ old('email') }}" 
+                                            required>
+
+                                            @if ($errors->has('email'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                            @endif
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <!-- message -->
+                            <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
+                                <label for="message" class="bmd-label-floating">Message*</label>
+                                <textarea type="text" class="form-control" rows="4" 
+                                        value="{{ old('text') }}" 
+                                        name="message" 
+                                        id="message" required>
+                                </textarea>
+
+                                @if ($errors->has('message'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('message') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-4 ml-auto mr-auto text-center">
+                                    <button class="btn btn-primary btn-raised" type="submit">
+                                        Send It
+                                    </button>
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-4 ml-auto mr-auto text-center">
+                                    <p>Field marked with an asterisk are mandatory.</p>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>--}}
+
+
+
         </div>
     </div>
 
